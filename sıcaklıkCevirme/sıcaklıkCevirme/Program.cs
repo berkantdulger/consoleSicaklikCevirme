9f1b31edeebe5ab8b30409244e5e0923e7755d6d
berkantdulger <98697821+berkantdulger@users.noreply.github.com>
2023-07-17 21:45:51 +0300

    Add files via upload

---
+++ sıcaklıkCevirme/sıcaklıkCevirme/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace sıcaklıkCevirme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double c;
+            double b;
+            Console.WriteLine("Lütfen Fahrenheit cinsinden sıcaklık değerini giriniz");
+            double f = Convert.ToDouble(Console.ReadLine());
+
+            b = f - 32;
+            c = b / 1.8;
+
+            Console.WriteLine("Celcius değeri " + c );
+            Console.ReadLine();
+        }
+    }
+}
